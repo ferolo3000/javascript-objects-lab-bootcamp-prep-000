e1add0c0f6cf8ed8ec93fb9bedfc6d2732d38f3f
Fernanda Romero <fernandaromero85@gmail.com>
2019-06-11 01:58:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,13 +16,3 @@ function deleteFromObjectByKey(object, key) {
   delete newObject.key
   return newObject
 }
-
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {key: value})
-}
-
-function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({}, object)
-  delete newObject.key
-  return newObject
-}
